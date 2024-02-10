ac3ff81a21dc908679142a00a52691b5c7535339
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2024-02-10 21:11:01 +0100

    main tag to app added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.module.css";
+import styles from "./App.module.css";
 import { Form } from "./components/Form/Form";
 
 function App() {
   return (
     <>
-      <Form />
+      <main>
+        <Form />
+      </main>
     </>
   );
 }
