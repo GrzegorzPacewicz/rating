29c1c57a59c76833e280ce74cceacbef25f480fa
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2024-02-10 21:11:34 +0100

    Form texts and submit button added

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,17 @@
+import styles from "./Form.module.css";
+
 export function Form() {
   return (
-    <>
-      <h2>Form </h2>
-    </>
+    <div className={styles.container}>
+      <h1 className={styles.title}>How did we do?</h1>
+      <p className={styles.text}>
+        Please let us know how we did with your support request. All feedback is
+        appreciated to help us improve our offering!
+      </p>
+      <form className={styles.form__container}>
+        <div className={styles.labels__container}>buttons</div>
+        <button className={styles.form__btn}>Submit</button>
+      </form>
+    </div>
   );
 }
