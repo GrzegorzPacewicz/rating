0486c12f3da0bf0f42b223a44ad80d8fb6f58e5d
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2024-02-11 10:21:56 +0100

    map function in labels. Labels to array

--- src/components/Confirmation/Confirmation.jsx
+++ src/components/Confirmation/Confirmation.jsx
@@ -9,14 +9,12 @@ export function Confirmation() {
         src={thanksIcon}
         alt="Thank You icon"
       />
-
       <p className={styles.selection}>You selected out of 5 </p>
       <h1 className={styles.title}>Thank You!</h1>
       <p className={styles.text}>
         We appreciate you taking the time to give a rating.
       </p>
       <p className={styles.text}>
-        {" "}
         If you ever need more support, don’t hesitate to get in touch!
       </p>
     </div>
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,6 +2,15 @@ import styles from "./Form.module.css";
 import starIcon from "../../assets/icon-star.svg";
 
 export function Form() {
+  const labels = [
+    { number: 1, value: 1 },
+    { number: 2, value: 2 },
+    { number: 3, value: 3 },
+    { number: 4, value: 4 },
+    { number: 5, value: 5 },
+    { number: 6, value: 6 },
+  ];
+
   return (
     <div className={styles.container}>
       <img className={styles.starIcon} src={starIcon} alt="star icon" />
@@ -12,12 +21,11 @@ export function Form() {
       </p>
       <form className={styles.formContainer}>
         <div className={styles.labelsContainer}>
-          <label className={styles.label}>1</label>
-          <label className={styles.label}>1</label>{" "}
-          <label className={styles.label}>1</label>{" "}
-          <label className={styles.label}>1</label>{" "}
-          <label className={styles.label}>1</label>{" "}
-          <label className={styles.label}>1</label>{" "}
+          {labels.map(({ number, value }) => (
+            <label key={number} value={value} className={styles.label}>
+              {number}
+            </label>
+          ))}
         </div>
         <button className={styles.formBtn}>Submit</button>
       </form>
