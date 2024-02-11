f868e7ab5c6e0fe1eb6de87d3b2b4e3b38c1a59d
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2024-02-11 15:37:43 +0100

    selectedValue added to confirmation

--- src/components/Confirmation/Confirmation.jsx
+++ src/components/Confirmation/Confirmation.jsx
@@ -1,7 +1,7 @@
 import styles from "./Confirmation.module.css";
 import thanksIcon from "../../assets/illustration-thank-you.svg";
 
-export function Confirmation() {
+export function Confirmation({ selectedValue }) {
   return (
     <div className={styles.container}>
       <img
@@ -9,7 +9,7 @@ export function Confirmation() {
         src={thanksIcon}
         alt="Thank You icon"
       />
-      <p className={styles.selection}>You selected out of 5 </p>
+      <p className={styles.selection}>You selected {selectedValue} out of 5 </p>
       <h1 className={styles.title}>Thank You!</h1>
       <p className={styles.text}>
         We appreciate you taking the time to give a rating.
