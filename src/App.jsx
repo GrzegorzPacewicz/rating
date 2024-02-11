a23e5195f4287412de964d472b342e6b2e2f6cb2
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2024-02-11 15:38:52 +0100

    HandleChange, handleSubmit functions added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
+import { useState } from "react";
 import "./App.module.css";
-import styles from "./App.module.css";
 import { Confirmation } from "./components/Confirmation/Confirmation";
 import { Form } from "./components/Form/Form";
 
 function App() {
+  const [formData, setFormData] = useState({
+    formValue: 0,
+    formSubmitted: false,
+  });
+
+  function handleChange(event) {
+    const { value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      formValue: value,
+    }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      formSubmitted: true,
+    }));
+  }
+
   return (
     <>
       <main>
-        <Form />
-        <Confirmation />
+        {!formData.formSubmitted ? (
+          <Form
+            formData={formData}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+          />
+        ) : (
+          <Confirmation selectedValue={formData.formValue} />
+        )}
       </main>
     </>
   );
